e6076b2984bd803d726c8d9b9e1b7895e690ed09
Andrew <apwicker@gmail.com>
2023-09-16 15:48:52 -0700

    Moved Cave files to Components Folder, Started Slider Code

--- client/components/MapDisplayModule.jsx
+++ client/components/MapDisplayModule.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cavern from './CaveGenerator.jsx';
 
 const MapDisplayModule = () => {
 	return (
 		<div className='mapDisplayModule'>
 			<h2>This is the map display Module</h2>
+			<Cavern />
 		</div>
 	);
 };
--- client/components/SlidersModule.jsx
+++ client/components/SlidersModule.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import e from 'cors';
+import React, { useState } from 'react';
 
 const SlidersModule = () => {
-	return (
-		<div className='slidersModule'>
-			<h2>This is the Sliders Module</h2>
-		</div>
-	);
+  const [sliderValue, setSliderValue] = useState(0);
+
+  const handleSliderChange = (e) => {
+    setSliderValue(e.target.value);
+  };
+
+  return (
+    <div className="slidersModule">
+      <h2>This is the Sliders Module</h2>
+      <input
+        id="sliderAlpha"
+        type="range"
+        min="0"
+        max="100"
+        value={sliderValue}
+        onChange={handleSliderChange}
+        className="sliderAlpha"
+      />
+    </div>
+  );
 };
 
 export default SlidersModule;
