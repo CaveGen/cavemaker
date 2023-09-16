48fec790b8a7ac4ad02a5dc916e84e07408bf59d
Andrew <apwicker@gmail.com>
2023-09-16 08:21:04 -0700

    Modularizing Components, Adjustments to Webpack & Package.json

---
+++ client/containers/MainContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthenticationForm from '../components/AuthenticationForm';
+
+const MainContainer = () => {
+	return (
+		<div className='authenticationDiv'>
+			<AuthenticationForm />
+		</div>
+	);
+};
+
+export default MainContainer;
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-    mode: 'development',
-    entry: './client/main.jsx',
-    output: {
-        path: path.join(__dirname, '/build'),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
+	mode: 'development',
+	entry: {
+		src: './client/App.jsx',
+	},
+	output: {
+		path: path.resolve(__dirname, '/build'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
 
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: ['@babel/env', '@babel/react']
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']  // Note the order here is important
-            }
-        ]
-    },
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				options: {
+					presets: ['@babel/env', '@babel/react'],
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'], // Note the order here is important
+			},
+		],
+	},
 
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'build'),
-        },
-        compress: true,
-        historyApiFallback: true, // This will redirect 404s to /index.html
-    },
+	devServer: {
+		host: 'localhost',
+		port: 8080,
+		hot: true,
+		static: {
+			directory: path.resolve(__dirname, 'build'),
+		},
+		proxy: {
+			'/': 'http://localhost:3000',
+		},
+		compress: true,
+		historyApiFallback: true, // This will redirect 404s to /index.html
+	},
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-            template: './client/index.html'
-        }),
-    ],
-}
-
-
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Development',
+			template: './client/index.html',
+		}),
+	],
+};
 
 // const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
