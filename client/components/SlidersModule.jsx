15193e69a764bd64f0c0693521219594d5015e7a
Andrew <apwicker@gmail.com>
2023-09-16 16:08:01 -0700

    Slider Module Stage 1 Implemented

--- client/components/SlidersModule.jsx
+++ client/components/SlidersModule.jsx
@@ -2,24 +2,64 @@ import e from 'cors';
 import React, { useState } from 'react';
 
 const SlidersModule = () => {
-  const [sliderValue, setSliderValue] = useState(0);
+  const [sliderValues, setSliderValues] = useState([0, 0, 0, 0]);
 
-  const handleSliderChange = (e) => {
-    setSliderValue(e.target.value);
+  const handleSliderChange = (event, index) => {
+    const newSliderValues = [...sliderValues];
+    newSliderValues[index] = event.target.value;
+    setSliderValues(newSliderValues);
   };
 
   return (
     <div className="slidersModule">
-      <h2>This is the Sliders Module</h2>
-      <input
-        id="sliderAlpha"
+      <div className="slider-container">
+        <h2>This is the Sliders Module</h2>
+        {sliderValues.map((value, index) => (
+          <div
+            key={index}
+            className="sliders"
+          >
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={value}
+              onChange={(e) => handleSliderChange(e, index)}
+              className="slider"
+            />{' '}
+            <p>
+              Slider {index + 1} value: {value}
+            </p>
+          </div>
+        ))}
+      </div>
+      {/* <input
         type="range"
         min="0"
         max="100"
         value={sliderValue}
         onChange={handleSliderChange}
-        className="sliderAlpha"
-      />
+        className="sliderBeta"
+      />{' '}
+      <p>Slider value: {sliderValue}</p>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={sliderValue}
+        onChange={handleSliderChange}
+        className="sliderGamma"
+      />{' '}
+      <p>Slider value: {sliderValue}</p>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={sliderValue}
+        onChange={handleSliderChange}
+        className="sliderDelta"
+      />{' '}
+      <p>Slider value: {sliderValue}</p> */}
     </div>
   );
 };
