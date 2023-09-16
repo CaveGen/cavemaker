d502b2dbf2d7ace00df505e38effde44aab20ee2
Andrew <apwicker@gmail.com>
2023-09-16 13:32:49 -0700

    CSS Grid Implementation, Component Files Created

---
+++ client/components/AuthenticationModule.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthenticationModule = () => {
+	return <div className='Authentication Module'>This is the Authentication Module</div>;
+};
+
+export default AuthenticationModule;
---
+++ client/components/MapDisplayModule.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MapDisplayModule = () => {
+	return (
+		<div className='mapDisplayModule'>
+			<h2>This is the map display Module</h2>
+		</div>
+	);
+};
+
+export default MapDisplayModule;
