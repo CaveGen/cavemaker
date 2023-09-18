86803d92ee04a69514025c143f2449a6411e0a0b
Andrew <apwicker@gmail.com>
2023-09-18 11:22:17 -0700

    Fixing Errors

--- client/components/SavedMaps.jsx
+++ client/components/SavedMaps.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 
-const SavedMaps = () => {
-  const mapDisplayGenerator = (mapName, mapData) => {
-    return (
-      <div
-        className="map-thumbnail"
-        id={mapName}
-      >
-        <p>{mapName}</p>
-        <svg>{mapData}</svg>
-      </div>
-    );
-  };
+const SavedMaps = ({ friendmaps, privateMaps }) => {
+	const combinedCollection = Object.assign({}, friendmaps, privateMaps);
 
-  // Checks if number of saved maps is greater than 0
-  if (savedMapsArray.length > 0) {
-    return Object.entries(savedMaps).forEach(([mapName, mapData]) => {
-      mapDisplayGenerator(mapName, mapData);
-    });
-    // If no saved maps, display message
-  } else {
-    return (
-      <div className="saved-maps-display">
-        <h2>No maps saved</h2>
-      </div>
-    );
-  }
+	const mapDisplayGenerator = (mapName, mapData) => {
+		return (
+			<div className='map-thumbnail' id={mapName}>
+				<p>{mapName}</p>
+				<svg>{mapData}</svg>
+			</div>
+		);
+	};
+
+	// Checks if number of saved maps is greater than 0
+	if (Object.keys(combinedCollection).length > 0) {
+		return Object.entries(combinedCollection).forEach(([mapName, mapData]) => {
+			mapDisplayGenerator(mapName, mapData);
+		});
+		// If no saved maps, display message
+	} else {
+		return (
+			<div className='saved-maps-display'>
+				<h2>No maps saved</h2>
+			</div>
+		);
+	}
 };
 
 export default SavedMaps;
